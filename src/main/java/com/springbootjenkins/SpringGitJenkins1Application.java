c66c5afb536add17da36cd69a09d3c5e9f72b63e
Tushar Singh Bisht <tusharbisht2011@gmail.com>
2022-01-04 15:58:49 +0530

    FIrst Commit

---
+++ src/main/java/com/springbootjenkins/SpringGitJenkins1Application.java
@@ -0,0 +1,13 @@
+package com.springbootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitJenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitJenkins1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/springbootjenkins/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.springbootjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
